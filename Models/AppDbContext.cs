d067c618b7f315029362ea16415eebecdfdea7f3
Juan Gallardo <juan.gallardo.ab@gmail.com>
2019-12-28 08:00:35 -0700

    Add AppDbContext

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace pieshop.Models
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Pie> Pies { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
--- Startup.cs
+++ Startup.cs
@@ -7,16 +7,26 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using pieshop.Models;
 
 namespace pieshop
 {
 public class Startup
 {
+	public IConfiguration Configuration { get; }
+
+	public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+
 	// This method gets called by the runtime. Use this method to add services to the container.
 	// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 	public void ConfigureServices(IServiceCollection services)
 	{
+		services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 		services.AddScoped<IPieRepository, MockPieRepository>();
 		services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 		services.AddControllersWithViews();
