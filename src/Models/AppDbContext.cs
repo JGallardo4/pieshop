b98a5d3405a4c82572e9c3eda517ae70efe6fac8
Juan Gallardo <juan.gallardo.ab@gmail.com>
2020-01-06 21:01:27 -0700

    Add shopping cart models

--- src/Models/AppDbContext.cs
+++ src/Models/AppDbContext.cs
@@ -11,6 +11,7 @@ namespace pieshop.Models
 
 				public DbSet<Pie> Pies { get; set; }
 				public DbSet<Category> Categories { get; set; }
+				public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
 
 				protected override void OnModelCreating(ModelBuilder modelBuilder)
 				{
